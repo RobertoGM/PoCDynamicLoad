91478c2900d2b419630fe388d08d51a614f80fb4
Magaña Mascuñana <miguel-angel.magana-mascunana@capgemini.com>
2019-02-22 14:10:05 +0100

    Added charts library and implemented change detection in parent view

--- src/app/portal/dashboard/dashboard.component.ts
+++ src/app/portal/dashboard/dashboard.component.ts
@@ -1,4 +1,12 @@
-import { Component, Type, ViewChildren, ComponentFactoryResolver, QueryList, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  Type,
+  ViewChildren,
+  ComponentFactoryResolver,
+  QueryList,
+  AfterViewInit,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { DocumentationBarComponent } from './documentation-bar/documentation-bar.component';
 import { OverviewChartComponent } from './overview-chart/overview-chart.component';
 import { UsageChartComponent } from './usage-chart/usage-chart.component';
@@ -7,27 +15,32 @@ import { AdDirective } from '../shared/ad.directive';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements AfterViewInit {
-
   components: Type<any>[] = [
     DocumentationBarComponent,
     OverviewChartComponent,
-    UsageChartComponent
+    UsageChartComponent,
   ];
 
   @ViewChildren(AdDirective) adHosts: QueryList<AdDirective>;
 
-  constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private changeDetector: ChangeDetectorRef,
+  ) {}
 
   ngAfterViewInit() {
     this.loadComponents();
+    this.changeDetector.detectChanges();
   }
 
   loadComponents(): void {
     this.adHosts.toArray().forEach((element: AdDirective, index: number) => {
-      const componentFactory = this.componentFactoryResolver.resolveComponentFactory(this.components[index]);
+      const componentFactory = this.componentFactoryResolver.resolveComponentFactory(
+        this.components[index],
+      );
 
       const viewContainerRef = element.viewContainerRef;
       viewContainerRef.clear();
--- src/app/portal/dashboard/documentation-bar/documentation-bar.component.ts
+++ src/app/portal/dashboard/documentation-bar/documentation-bar.component.ts
@@ -3,13 +3,134 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-documentation-bar',
   templateUrl: './documentation-bar.component.html',
-  styleUrls: ['./documentation-bar.component.scss']
+  styleUrls: ['./documentation-bar.component.scss'],
 })
 export class DocumentationBarComponent implements OnInit {
-
-  constructor() { }
+  private financial: any;
+  private overview: any;
+  private services: any;
+  constructor() {}
 
   ngOnInit() {
-  }
+    this.financial = {
+      labels: [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+      ],
+      datasets: [
+        {
+          label: 'Expenses per month in M',
+          data: [65, 59, 80, 81, 56, 55, 40, 65, 32, 45, 78, 90],
+          fill: false,
+          backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+          borderColor: ['rgb(255, 99, 132)'],
+          borderWidth: 1,
+        },
+        {
+          label: 'Expected expenses in M',
+          data: [75, 79, 80, 81, 46, 35, 40, 55, 22, 45, 58, 90],
+          fill: false,
+          backgroundColor: ['rgba(255, 159, 64, 0.2)'],
+          borderColor: ['rgb(255, 159, 64)'],
+          borderWidth: 1,
+        },
+      ],
+    };
 
+    this.overview = {
+      labels: [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+      ],
+      datasets: [
+        {
+          label: 'Requests per month',
+          data: [65, 59, 80, 81, 56, 55, 40, 81, 56, 55, 40, 80],
+          fill: false,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)',
+          ],
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)',
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+    this.services = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'My First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)',
+          ],
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+  }
 }
